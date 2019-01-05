6d5031b572029a5e7c55e26fde49b61b3fd211d5
bantyK <bantyp92@gmail.com>
2019-01-05 18:46:58 +0530

    Comments added in ChartsFragment

--- CryptoStats/app/src/main/java/banty/com/cryptostats/charts/ChartsFragment.kt
+++ CryptoStats/app/src/main/java/banty/com/cryptostats/charts/ChartsFragment.kt
@@ -15,20 +15,24 @@ import com.github.mikephil.charting.data.LineData
  * Created by Banty on 05/01/19.
  *
  * Fragment to display the chart.
- * All the information required to plot the chart is passed on bundle.
+ * All the information required to plot the chart is passed on through the bundle.
  * Uses MPAndroidChart library for displaying the charts : https://github.com/PhilJay/MPAndroidChart
  */
 class ChartsFragment : Fragment(), ChartsFragmentMVPContract.View {
 
-    val TAG = "ChartsFragment"
+    private val logTag = "ChartsFragment"
 
-    lateinit var lineChart: LineChart
+    private lateinit var lineChart: LineChart
 
     private var presenter: ChartsFragmentMVPContract.Presenter? = null
 
     companion object {
         const val PARCEL_KEY = "charts_data"
 
+        /*
+        * static function which will be used by the activity to create a new instance of the
+        * fragment with the @param BitcoinApiResponseModel passed as bundle.
+        * */
         fun newInstance(bitcoinApiResponseModel: BitcoinApiResponseModel): ChartsFragment {
             val args = Bundle()
             args.putParcelable(PARCEL_KEY, bitcoinApiResponseModel)
@@ -47,10 +51,16 @@ class ChartsFragment : Fragment(), ChartsFragmentMVPContract.View {
         return view
     }
 
+    /**
+     * Initializes the presenter
+     * */
     private fun initPresenter() {
         presenter = ChartsFragmentPresenter(this)
     }
 
+    /**
+     * Intializes the views
+     * */
     private fun initViews(view: View) {
         lineChart = view.findViewById(R.id.linechart)
     }
@@ -60,15 +70,21 @@ class ChartsFragment : Fragment(), ChartsFragmentMVPContract.View {
         presenter?.setChartData(getChartsDataFromArguments())
     }
 
+    /**
+     * Called by Presenter when chart is ready to be displayed with the the
+     * required information
+     * */
     override fun showChart(data: LineData) {
         lineChart.data = data
         lineChart.setDescription(presenter?.getDescription(getChartsDataFromArguments()))
-
     }
 
+    /**
+     * Returns the BitcoinApiResponseModel from arguments (set by Activity while creating the newInstance)
+     * */
     private fun getChartsDataFromArguments(): BitcoinApiResponseModel? {
         val bitCoinChartsData = arguments?.getParcelable<BitcoinApiResponseModel>(PARCEL_KEY)
-        Log.d(TAG, "Response : values size : ${bitCoinChartsData?.values?.size}")
+        Log.d(logTag, "Response : values size : ${bitCoinChartsData?.values?.size}")
         return bitCoinChartsData
     }
 }
